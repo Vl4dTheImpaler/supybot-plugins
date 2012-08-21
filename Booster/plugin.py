ef8e5c97d2a1a9b1a7a77430cdb4f9cb440cf458
Vlad <theimpaler@tormail.org>
2012-08-21 14:33:39 -0400

    Added Booster plugin

---
+++ Booster/plugin.py
@@ -0,0 +1,37 @@
+
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+import string, sys, time, urllib2, cookielib, re, random, threading, socket, os, subprocess
+import pbj
+
+
+class Booster(callbacks.Plugin):
+    """Add the help for "@plugin help Booster" here
+    This should describe *how* to use this plugin."""
+    threaded = True
+
+    def makebooster(self, irc, msg, args, text):
+        reload(pbj)
+        nick = msg.nick
+        channell = msg.args[0]
+        if nick in irc.state.channels[channell].halfops:
+            capability='halfops'
+            if not ircdb.checkCapability(msg.prefix, capability):
+                irc.errorNoCapability(capability, Raise=True)
+        irc.reply('Booster is on its way!')      
+        req = urllib2.Request("http://designature.se/acta/?url=%s&plain" % text)
+        responselol = urllib2.urlopen(req)
+        contentsz = responselol.read()
+        rawr = pbj.pastehtml(contentsz)
+        rawr = rawr.replace('/view/', '/raw/')
+        irc.reply(rawr)
+    makebooster = wrap(makebooster, ['anything'])
+
+Class = Booster
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
---
+++ Booster/test.py
@@ -0,0 +1,10 @@
+
+###
+
+from supybot.test import *
+
+class BoosterTestCase(PluginTestCase):
+    plugins = ('Booster',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
